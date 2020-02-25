d4a805e1bf1bda86da3bdf2a44d8dd0c11f843d8
jayC <jaywaynejuly@gmail.com>
2020-02-25 00:27:14 -0500

    Initial set up

---
+++ src/test/java/example/HelloTekLab.java
@@ -0,0 +1,12 @@
+package example;
+
+public class HelloTekLab {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello TekLab Students");
+		System.out.println(" Let's change the world");
+
+	}
+
+}
