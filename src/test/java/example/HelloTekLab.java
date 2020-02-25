96bbff96723b1d5cfdc2c0a5818913fdd516461f
jayC <jaywaynesky@gmail.com>
2020-02-25 02:11:02 -0500

    add "today is tuesday" message

--- src/test/java/example/HelloTekLab.java
+++ src/test/java/example/HelloTekLab.java
@@ -6,6 +6,9 @@ public class HelloTekLab {
 		
 		System.out.println("Hello TekLab Students");
 		System.out.println(" Let's change the world");
+		
+		//new commit
+		System.out.println("Today is Tuesday");
 
 	}
 
