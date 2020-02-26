f4525321607ddbf454345250904120676f26610c
yang22042 <yang22042@DESKTOP-5A05A5P>
2020-02-26 13:23:08 -0500

    changes

---
+++ src/test/java/example/HelloTekLab2.java
@@ -0,0 +1,69 @@
+package example;
+
+public class HelloTekLab2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello TekLab Students");
+		System.out.println(" Let's change the world");
+		
+		System.out.println("--------Change this line and and ****");
+		System.out.println("Showing how to commit ");
+		
+
+		System.out.println("Hello Chris");
+		System.out.println("something");
+		// new commit
+		System.out.println("Today is Tuesday");
+
+		
+		String temp=" ";
+		for(int i=0; i<6;i++) {
+			temp=temp+"*";
+			System.out.println(temp);
+
+		}
+
+		
+		//*************Christian Changed code here:
+		
+		System.out.println("                                  .");
+		System.out.println("                               c$P'   ..");
+		System.out.println("                              <$     c$$$r");
+		System.out.println("                         ,c$F'$$,$h c$$,P  .z$>");
+		System.out.println("                     ,c$$$$'  <$$$$.$$$\",cdP\"             ,c-");
+		System.out.println("                     $$\"<$$   $$ $$h,$$P\"            .zc$P\"       zJ$$$c,");
+		System.out.println("                     ?  $$$   $$.`?\"\'            ,c$$$P\"        ,$$$P$$$$.");
+		System.out.println("                        $$$   ?\"            .,c$$$P\"'    ,c$$c  $$$P $$$$>");
+		System.out.println("                        $$F    .zcr     .,c$$$P\"'    .,,,$$$$$h<$$$h `$$\"");
+		System.out.println("                  _,c$\"       J$$$% .,c$$$P\"'       c$$$$$$$$$$`$$$$c");
+		System.out.println("              .,c$$$$'        $$$$$$$$P\"'    .zJhc J$$$$$P $$$$ `$$$$h.,;J");
+		System.out.println("          .zc$$$$$$$'       ,c$$$$P\"        z$$$$$h`\"?$$$F $$$$  `$$$$$.\"");
+		System.out.println("      .,c$$$$$$$$$$'    ,c$$$P$$$$   <$c    <$$\"\"?$$F <$$$  $$$P ,c,?$$$$");
+		System.out.println("     \"?$$$$$$$$$$$$     $??\"  $$$$   $$$c $$$  `$$$ <$$$  $$$F$$\"  $$$$");
+		System.out.println("        \"?$$$$$$$$F           $$$$   $P$$c $$$   $$$ J$$$  $$$F$F   $$$$");
+		System.out.println("           `?$$$$$F     .,.   $$$$  .$>?$$c?$$  <$$$ $$$$  $$$F$h. z$$$%");
+		System.out.println("            <$$$$$>   z$$$$$r $$$$  J$  ?$$,$$$$$$$$ $$$$  $$P,$$$$$$$'");
+		System.out.println("            <$$$$$>  J$$P\"$$$ $$$$  $P   $$h?$$$$$$F $$$$  `\" ?$$$$P'");
+		System.out.println("            <$$$$$> J$$\"  $$P<$$$$ ,P,$h `$$`$$$$$P  ??''");
+		System.out.println("            $$$$$$'<$$$  c$$'<$$$$ $h$$$>J$$.$$$$F");
+		System.out.println("            $$$$$$'$$$$c$$P' J$$$$$$$$$zc$$$ `''");
+		System.out.println("  ,c$$$$c,  $$$$$$'$$$$P\"\"  J$$$$$$$'$$$$$P'");
+		System.out.println(" z$$$$$$$$. $$$$$$ $$$$$,.z$$F$$$$$F  `\"''");
+		System.out.println("<$$$$$$$$$> $$$$$$ <$$$$$$$$P ?$$P'");
+		System.out.println("$$$$$$$$$$  $$$$$$  ?$$$$$$P");
+		System.out.println("$$$$$?$$F  ,$$$$$P   '?$$P'");
+		System.out.println("$$$$$     ,$$$$$P");
+		System.out.println("`$$$$$cccd$$$$$P");
+		System.out.println(" `$$$$$$$$$$$$\"");
+		System.out.println("  `?$$$$$$$P\"");
+		System.out.println("    *****");
+		
+		//*********************************************End of Changes*********************************************
+		
+		System.out.println("Thanks for working with me Chris");
+
+	}
+
+	}
+
