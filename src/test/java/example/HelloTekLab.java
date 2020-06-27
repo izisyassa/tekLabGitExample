f2c3385323c7d775f20ba22a40d2043a6d6c2607
jayC <jaywaynesky@gmail.com>
2020-06-27 12:14:45 -0400

    added beforeMethod

--- src/test/java/example/HelloTekLab.java
+++ src/test/java/example/HelloTekLab.java
@@ -1,5 +1,8 @@
 package example;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
 public class HelloTekLab {
 
 	public static void main(String[] args) {
@@ -21,7 +24,14 @@ public class HelloTekLab {
 		System.out.println("another code");
 		
 		System.out.println("changes in jc loginpage verify");
+		System.out.println(" seconnde ");
 
+	}
+	
+	@BeforeMethod
+	public void setUp() {
+		String driverpath = System.getProperty("user.dir") + "/src/test/resources/drivers/chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", driverpath);
 	}
 
 }
