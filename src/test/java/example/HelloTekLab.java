1d27b9313002a54f06ed9d7d938f61ddc43a45c6
jayC <jaywaynesky@gmail.com>
2020-06-27 12:09:11 -0400

    added new change in loginpage verify

--- src/test/java/example/HelloTekLab.java
+++ src/test/java/example/HelloTekLab.java
@@ -19,6 +19,8 @@ public class HelloTekLab {
 		System.out.println("Jay Green");
 		System.out.println("Robert Blue");
 		System.out.println("another code");
+		
+		System.out.println("changes in jc loginpage verify");
 
 	}
 
