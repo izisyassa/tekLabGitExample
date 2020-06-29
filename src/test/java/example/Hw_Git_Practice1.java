5d15bef358d6e4a44e189233368b3fe76e5f805b
Priscilla <Priscilla@LAPTOP-VACVDJ1K.fios-router.home>
2020-06-29 18:07:08 -0400

    added updated For-Loop with double exclamation marks

---
+++ src/test/java/example/Hw_Git_Practice1.java
@@ -0,0 +1,17 @@
+package example;
+
+public class Hw_Git_Practice1 {
+
+	public static void main(String[] args) {
+
+		String[] cars = { "Tesla", "Mercedes Benz", "BMW", "Lexus" };
+		for (String i : cars) {
+			System.out.println(" Niceeeee :-) ");
+			System.out.println(i);
+			System.out.println("!!");
+
+		}
+
+	}
+}
+
