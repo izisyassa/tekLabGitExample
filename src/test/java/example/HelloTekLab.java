f56334046e2d22245f2466e279a02e704759924f
jayC <jaywaynesky@gmail.com>
2020-10-27 17:32:10 -0400

    Updated the project on 10/27/2020

--- src/test/java/example/HelloTekLab.java
+++ src/test/java/example/HelloTekLab.java
@@ -8,27 +8,7 @@ public class HelloTekLab {
 	public static void main(String[] args) {
 
 		System.out.println("Hello TekLab Students");
-		System.out.println(" Let's change the world");
 		
-		System.out.println("--------Change this line and and ****");
-		System.out.println("Showing how to commit ");
-		System.out.println("clean up code");
-		System.out.println(" another code I write");
-		System.out.println("wafa white");
-		
-		
-		System.out.println("Priscilla 7");
-
-		System.out.println("Moe");
-		System.out.println("Jay Green");
-		System.out.println("Robert Blue");
-
-		System.out.println(" change in MK logon ");
-
-		System.out.println("another code");
-		
-		System.out.println("changes in jc loginpage verify");
-		System.out.println(" seconnde ");
 
 	}
 	
