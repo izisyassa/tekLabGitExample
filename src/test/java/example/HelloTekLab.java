48f68d725b963ff8129cf3286c20743ff0ea5526
jayC <jaywaynesky@gmail.com>
2020-10-31 11:03:36 -0400

    added a good method in HelloTaklab

--- src/test/java/example/HelloTekLab.java
+++ src/test/java/example/HelloTekLab.java
@@ -9,6 +9,9 @@ public class HelloTekLab {
 
 		System.out.println("Hello TekLab Students");
 		
+		System.out.println("This is Peter's change");
+		
+		
 
 	}
 	
@@ -16,6 +19,10 @@ public class HelloTekLab {
 	public void setUp() {
 		String driverpath = System.getProperty("user.dir") + "/src/test/resources/drivers/chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", driverpath);
+	}
+	
+	public void goodMethod() {
+		System.out.println("this is a new method");
 	}
 
 }
