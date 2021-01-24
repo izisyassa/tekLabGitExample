2b1b5ab5590fe98b4c384e3488163a8d2a208562
isish <isish@MSI.fios-router.home>
2021-01-24 14:08:22 -0500

    Added a meaingful massage ( add new eclips)

--- src/test/java/example/HelloTekLab.java
+++ src/test/java/example/HelloTekLab.java
@@ -8,8 +8,8 @@ public class HelloTekLab {
 	public static void main(String[] args) {
 
 		System.out.println("Hello TekLab Students");
-		
-
+		System.out.println("Izis branch is here");
+		System.out.println("today is sunday");
 	}
 	
 	@BeforeMethod
